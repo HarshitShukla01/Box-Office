0077d22b2dbdf62b1a447c9d3fd75b11ecad5a6c
HarshitShukla01 <harshitshukla258@gmail.com>
2021-07-03 15:39:22 +0530

    update deploy

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
   <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
