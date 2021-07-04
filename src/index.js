7c3c47af58a88819bdfe9f0f2b7e5921cabf2c60
Harshit Shukla <75080333+HarshitShukla01@users.noreply.github.com>
2021-07-04 16:13:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM,{BrowserRouter} from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import { BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
